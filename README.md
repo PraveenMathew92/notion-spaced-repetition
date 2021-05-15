0b8acf338cbb4e1b808decd36035b231eeeddc73
Praveen Mathew <praveen.mathew92@outlook.com>
2021-05-15 17:31:03 +0530

    Set up Test

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spaced Repetation Using Notion
+
+## Notion API
+## Langugae: Javascript (Node js)
---
+++ tests/index.test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('Smoke Test', () => {
+    it('tautology', () => {
+        expect(true).to.be.true;
+    })
+})
